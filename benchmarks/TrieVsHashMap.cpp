d2333070ffb8ac4e323ba8ef245f03284f8320ec
Piotr Tański <piotr.tanski@mudita.com>
2022-02-02 19:09:16 +0100

    Added second dictionary.

--- benchmarks/TrieVsHashMap.cpp
+++ benchmarks/TrieVsHashMap.cpp
@@ -2,12 +2,19 @@
 
 #include <t9trie/DictionaryLoader.hpp>
 
-static void TrieDictionaryLoad(benchmark::State& state) {
+static void TrieDictionaryLoad10k(benchmark::State& state) {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./english.txt", T9::DictionaryImplementation::Trie));
+        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./10k_english.txt", T9::DictionaryImplementation::Trie));
     }
 }
-BENCHMARK(TrieDictionaryLoad);
+BENCHMARK(TrieDictionaryLoad10k);
+
+static void TrieDictionaryLoad466k(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./466k_english.txt", T9::DictionaryImplementation::Trie));
+    }
+}
+BENCHMARK(TrieDictionaryLoad466k);
 
 static void TrieDictionaryInsert(benchmark::State& state) {
     std::stringstream ss;
@@ -19,19 +26,26 @@ static void TrieDictionaryInsert(benchmark::State& state) {
 BENCHMARK(TrieDictionaryInsert);
 
 static void TrieDictionarySearch(benchmark::State& state) {
-    auto dict = T9::DictionaryLoader::loadFromFile("./english.txt", T9::DictionaryImplementation::Trie);
+    auto dict = T9::DictionaryLoader::loadFromFile("./10k_english.txt", T9::DictionaryImplementation::Trie);
     for (auto _ : state) {
         benchmark::DoNotOptimize(dict->search({4, 3, 5, 5, 6}));
     }
 }
 BENCHMARK(TrieDictionarySearch);
 
-static void HashMapDictionaryLoad(benchmark::State& state) {
+static void HashMapDictionaryLoad10K(benchmark::State& state) {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./english.txt", T9::DictionaryImplementation::HashMap));
+        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./10k_english.txt", T9::DictionaryImplementation::HashMap));
     }
 }
-BENCHMARK(HashMapDictionaryLoad);
+BENCHMARK(HashMapDictionaryLoad10K);
+
+static void HashMapDictionaryLoad466k(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(T9::DictionaryLoader::loadFromFile("./466k_english.txt", T9::DictionaryImplementation::HashMap));
+    }
+}
+BENCHMARK(HashMapDictionaryLoad466k);
 
 static void HashMapDictionaryInsert(benchmark::State& state) {
     std::stringstream ss;
@@ -43,7 +57,7 @@ static void HashMapDictionaryInsert(benchmark::State& state) {
 BENCHMARK(HashMapDictionaryInsert);
 
 static void HashMapDictionarySearch(benchmark::State& state) {
-    auto dict = T9::DictionaryLoader::loadFromFile("./english.txt", T9::DictionaryImplementation::HashMap);
+    auto dict = T9::DictionaryLoader::loadFromFile("./10k_english.txt", T9::DictionaryImplementation::HashMap);
     for (auto _ : state) {
         benchmark::DoNotOptimize(dict->search({4, 3, 5, 5, 6}));
     }
